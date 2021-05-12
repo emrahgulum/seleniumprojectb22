08ba51b8d6ca1ad43298539afc15ca0d24b1b855
emrahgulum <82914905+emrahgulum@users.noreply.github.com>
2021-05-11 21:20:33 -0400

    Initial commit

---
+++ src/test/java/com/cybertek/tests/day1_selenium_intro/Yahoo_Practice.java
@@ -0,0 +1,2 @@
+package com.cybertek.tests.day1_selenium_intro;public class Yahoo_Practice {
+}
---
+++ src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/P2_CybertekUrlVerification.java
@@ -0,0 +1,2 @@
+package com.cybertek.tests.day2_locators_getText_getAttribute;public class P2_CybertekUrlVerification {
+}
