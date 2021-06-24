8645785664ee4c1d3474c194b1d01ae5d61ffa77
emrahgulum <82914905+emrahgulum@users.noreply.github.com>
2021-06-23 21:06:10 -0400

    added some drive utils

---
+++ src/test/java/com/cybertek/pages/LibraryLoginPage.java
@@ -0,0 +1,23 @@
+package com.cybertek.pages;
+
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LibraryLoginPage {
+
+    public LibraryLoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "inputEmail")
+    public WebElement inputEmail;
+
+    @FindBy(id = "inputPassword")
+    public WebElement inputPassword;
+
+    @FindBy(xpath = "/button[text()='Sign in']")
+    public WebElement signInButton;
+
+}
